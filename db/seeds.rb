1f31b380700061942def569ab5c226f4f66639a0
Daniel Suarez <suarez.daniel101@gmail.com>
2023-04-26 17:19:31 -0400

    creating seed data in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ comic_3 = Comic.create({ title: "Green Lantern", description: "Beware my power, 
 comic_4 = Comic.create({ title: "Daredevil", description: "The man without fear." })
 
 
-comic_1 = Comic.create({ title: "Superman", description: "Man of steel leaps tall buildings in a single bound." })
-comic_2 = Comic.create({ title: "Spiderman", description: "Your friendly neighborhood spiderman." })
-comic_3 = Comic.create({ title: "Green Lantern", description: "Beware my power, Green Lantern's Light!" })
-comic_4 = Comic.create({ title: "Daredevil", description: "The man without fear." })
+reader_1 = Reader.create({ name: "Agus", email: "ag@own.com" })
+reader_2 = Reader.create({ name: "Carlos", email: "cl@twist.com" })
+reader_3 = Reader.create({ name: "Albert", email: "al@unknown.com" })
+reader_4 = Reader.create({ name: "Wayne", email: "way@sen.com" })
