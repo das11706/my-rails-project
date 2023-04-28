bb727930912b7b9cf2d3c6da97d4dcf6dd8833a4
Daniel Suarez <suarez.daniel101@gmail.com>
2023-04-27 20:19:52 -0400

    added strong params to readers controller

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -8,4 +8,19 @@ class ReadersController < ApplicationController
   def new
     @reader = Reader.new
   end
+
+  def create
+    @reader = Reader.new(reader_params)
+    if @reader.save
+      redirect_to @reader
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def reader_params
+      params.require(:reader).permit(:name, :email, :password, :password_confirmation)
+    end
 end
