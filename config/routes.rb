5c94ad344bdf2435d4d2ce3480f8a9cc20f31bc3
Daniel Suarez <suarez.daniel101@gmail.com>
2023-04-29 15:13:52 -0400

    add sessions routes to routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get 'signup' => 'readers#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
