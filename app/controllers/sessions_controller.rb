0875c3041af931661e38ad80ba6b2ccbf906210c
Daniel Suarez <suarez.daniel101@gmail.com>
2023-04-29 21:05:21 -0400

    add log_in and current_user helper methods to sessions_helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,21 @@
 class SessionsController < ApplicationController
+  
   def new
-
   end
 
   def create
-
+    reader = Reader.find_by(email: params[:session][:email].downcase)
+    if reader && reader.authenticate(params[:session][:password])
+      log_in reader
+      redirect_to reader
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
-  def destroy_all
-    
+  def destroy
+
   end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,10 @@
 module SessionsHelper
+
+  def log_in(reader)
+    session[:reader_id] = reader.id  
+  end
+
+  def current_user
+    @current_user ||= Reader.find_by(id: session[:user_id])
+  end
 end
