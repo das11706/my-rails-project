93a62b712b89f25ea866b5b3356c4a0ea78efcbf
Daniel Suarez <suarez.daniel101@gmail.com>
2023-05-04 12:18:45 -0400

    create methods or Contributors controller

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,2 +1,29 @@
 class ContributorsController < ApplicationController
+
+  def index
+    @contributors = Contributor.all 
+  end
+
+  def new
+    @contributor = Contributor.new 
+  end
+
+  def create
+    @contributor = Contributor.new(contributor_params)
+    if @contributor.save
+      redirect_to @contributor
+    else
+      render :new
+    end
+  end
+
+  def show
+    @contributor = Contributor.find(params[:id])
+  end
+
+  private
+    def contributor_params
+      params.require(:contributor).permit(:name, :title, :email, :password)
+    end
+    
 end
