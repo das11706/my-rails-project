8847d5939d1a960376ebd08ceff23b017056ce0c
Daniel Suarez <suarez.daniel101@gmail.com>
2023-05-04 12:46:06 -0400

    create methods for Contributors Controller

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,29 +1,28 @@
 class ContributorsController < ApplicationController
 
-  def index
-    @contributors = Contributor.all 
+  def show
+    @contributor = Contributor.find(params[:id])
+    # debugger
   end
 
   def new
-    @contributor = Contributor.new 
+    @contributor = Contributor.new
   end
 
   def create
     @contributor = Contributor.new(contributor_params)
     if @contributor.save
+      log_in @contributor
+      flash[:success] = "Welcome to the Comics Contributor App!"
       redirect_to @contributor
     else
       render :new
     end
   end
 
-  def show
-    @contributor = Contributor.find(params[:id])
-  end
-
   private
     def contributor_params
-      params.require(:contributor).permit(:name, :title, :email, :password)
+      params.require(:contributor).permit(:name, :title, :email, :password, :password_confirmation)
     end
     
 end
